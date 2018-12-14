a27989fff7829a3df614a70253bca6f4391c3683
Unknown <jonas.angholt@gmail.com>
2018-12-14 13:13:56 +0100

    Create puzzle solver using reflection

--- AdventCode/PuzzleSolverFactory.cs
+++ AdventCode/PuzzleSolverFactory.cs
@@ -1,8 +1,4 @@
-﻿using AdventCode.Day1;
-using AdventCode.Day2;
-using AdventCode.Day3;
-using AdventCode.Day4;
-using AdventCode.Day5;
+﻿using System;
 
 namespace AdventCode
 {
@@ -11,21 +7,10 @@ namespace AdventCode
         public static PuzzleSolver Create(int day)
         {
             var filePath = $"Day{day}\\input.txt";
-            switch (day)
-            {
-                case 1:
-                    return new Day1PuzzleSolver(filePath);
-                case 2:
-                    return new Day2PuzzleSolver(filePath);
-                case 3:
-                    return new Day3PuzzleSolver(filePath);
-                case 4:
-                    return new Day4PuzzleSolver(filePath);
-                case 5:
-                    return new Day5PuzzleSolver(filePath);
-                default:
-                    return null;
-            }
+            var puzzleSolverTypeName = $"AdventCode.Day{day}.Day{day}PuzzleSolver";
+            var puzzleSolverType = Type.GetType(puzzleSolverTypeName);
+
+            return Activator.CreateInstance(puzzleSolverType, filePath) as PuzzleSolver;
         }
     }
 }
