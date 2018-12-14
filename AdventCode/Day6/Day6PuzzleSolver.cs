1811166052c32961f22061874e9a6521534f8ea8
Unknown <jonas.angholt@gmail.com>
2018-12-14 14:43:56 +0100

    Day 6 initial setup

---
+++ AdventCode/Day6/Day6PuzzleSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventCode.Day6
+{
+    public class Day6PuzzleSolver : PuzzleSolver
+    {
+        public Day6PuzzleSolver(string inputFileName) : base(inputFileName)
+        {
+        }
+
+        public override void SolvePuzzle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
